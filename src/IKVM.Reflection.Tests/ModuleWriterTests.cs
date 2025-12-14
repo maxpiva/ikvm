3670fef7f363ec00c2aae9c7662a3cd1b96dd3fb
Jerome Haltom <jhaltom@alethic.solutions>
2025-12-14 13:51:00 -0600

    Update MSTest.Sdk to last 3 version. Need Core 6. Update NoTargets. Update a few System. packages.

--- src/IKVM.Reflection.Tests/ModuleWriterTests.cs
+++ src/IKVM.Reflection.Tests/ModuleWriterTests.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
-using System.Runtime.InteropServices;
 using System.Text;
 
 using FluentAssertions;
